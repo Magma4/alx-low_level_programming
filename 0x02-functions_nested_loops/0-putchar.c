1d5b67a6470bea9a6e27e39db9eac44c92ff90d8
Raymond <raymondamoateng@gmail.com>
2022-04-12 09:29:41 -0700

    Debugged

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,10 +8,10 @@
 */
 int main(void)
 {
-	char word[7] = "Putchar";
+	char word[8] = "_putchar";
 	int i;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
