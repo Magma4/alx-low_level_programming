2b1510193640dd7c4472e88017e66f2a1f444506
Raymond <raymondamoateng@gmail.com>
2022-04-12 09:09:04 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "putchar.h"
+
+/**
+* main - Check decription
+* Description: It prints the Putchar, followed by a new line.
+* Return: 0.
+*/
+int main(void)
+{
+	char word[7] = "Putchar";
+	int i;
+
+	for (i = 0; i < 7; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
